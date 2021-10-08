a51428b2326a7f0f6ce0a2489a0f5abd7816cb3e
Thomas <thomasjungcoe@gmail.com>
2021-10-07 19:25:41 -0500

    auto commit

--- auto_commit_push.sh
+++ auto_commit_push.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 #<<"C:/Users/Thomas H-CTR Jung/Desktop/Study/Git_Tutorial/repo2/git_revisit">> = branch I am pushing to
-echo 'Ident,runway,city,state,type,predicted_MTBO,projected_MTBO,last_MTBO,hours_since_last_outage,region,tier\n' | cat - tester2.txt > temp && mv temp tester2.txt
 git status
 git add .
 git commit -m "auto commit" $1
