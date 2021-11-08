3e795f9f8e6e8f1648de835d393666a386c36c4c
Thomas <thomasjungcoe@gmail.com>
2021-11-07 22:17:01 -0600

    auto commit

--- autonous_reporting_system_script.sh
+++ autonous_reporting_system_script.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 #<<"C:/Users/Thomas H-CTR Jung/Desktop/Study/Git_Tutorial/repo2/git_revisit">> = branch I am pushing to
 
-C:\Users\"Thomas H-CTR Jung"\Desktop\CoS\"CoS Analyze"\"CoS Analyze"\Debug_v1\"CoS Analyze.exe"
-
-# press enter or what fuak~~!
+Desktop/"CoS Analyze.exe"
 
 cd C:/Users/"Thomas H-CTR Jung"/Desktop/front/"Git Repo"
 
