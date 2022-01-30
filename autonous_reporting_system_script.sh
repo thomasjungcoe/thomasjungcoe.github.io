e73627ae889bc492ac2f360d9b4742359fb75842
Thomas <thomasjungcoe@gmail.com>
2022-01-30 17:43:48 -0600

    auto commit

--- autonous_reporting_system_script.sh
+++ autonous_reporting_system_script.sh
@@ -3,7 +3,7 @@
 
 C:/Users/"Thomas H-CTR Jung"/Desktop/CoS/"CoS Analyze"/"CoS Analyze"/Debug_v1/"CoS Analyze.exe"
 
-cd C:/Users/"Thomas H-CTR Jung"/Desktop/Study/Git_Tutorial/repo2/thomasjungcoe.github.io
+cd C:/Users/"Thomas H-CTR Jung"/Desktop/Git Repo2/thomasjungcoe.github.io
 
 
 ./auto_commit_push.sh
